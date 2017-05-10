4b6345d360a96d83a679ae55134f3d0de2c9ced9
Wush Wu <wush978@gmail.com>
2017-05-11 00:45:03 +0800

    use file extension to switch binary mode

--- embeddings/load_embeddings.py
+++ embeddings/load_embeddings.py
@@ -6,8 +6,9 @@ def load_embeddings(EMBEDDING_FILE, EMBEDDING_DIM):
   ## index word vectors
   ########################################
   print('Indexing word vectors')
+  isbin = EMBEDDING_FILE.endswith(".bin")
   word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, \
-        binary=False)
+        binary=isbin)
   print('Found %s word vectors of word2vec' % len(word2vec.vocab))
   return word2vec
 
